bd71b5eaa2e00fb2f26d4d97272b2c5da9d0cd35
Matthias Schmitt <mschmitt@db12.de>
2011-07-11 17:25:28 +0200

    junit

---
+++ src/test/java/de/db12/krimispiel/LineboardTest.java
@@ -0,0 +1,14 @@
+package de.db12.krimispiel;
+
+import org.junit.Test;
+
+import de.db12.krimispiel.model.KrimiGame;
+
+public class LineboardTest {
+
+	@Test
+	public void test() {
+		new KrimiGame().start();
+	}
+
+}
